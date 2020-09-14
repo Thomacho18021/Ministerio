38d0ccb4817dde3cb7f0f40044efaf505ba1656e
Thomas Carrillo <Thomascarrillogonzalez@gmail.com>
2020-09-14 20:05:54 -0300

    6 Commit: Se agrega boton de salir a action bar y se programa para cerrar app

---
+++ app/src/main/java/com/srcarrillo/ministerio/AjustesActivity.java
@@ -0,0 +1,17 @@
+package com.srcarrillo.ministerio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+public class AjustesActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ajustes);
+
+    }
+}
--- app/src/main/java/com/srcarrillo/ministerio/MainActivity.java
+++ app/src/main/java/com/srcarrillo/ministerio/MainActivity.java
@@ -1,13 +1,16 @@
 package com.srcarrillo.ministerio;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -62,5 +65,18 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Toast.makeText(this, "Ajustes", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.action_salir:
+                System.exit(0);
+                return true;
+        }
+        return false;
     }
 }
