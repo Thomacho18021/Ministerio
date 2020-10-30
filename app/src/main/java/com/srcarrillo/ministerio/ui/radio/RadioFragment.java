e983316e38544a5f08de85bd4bbcd8400ea0dd50
Thomas Carrillo <Thomascarrillogonzalez@gmail.com>
2020-10-30 19:42:39 -0300

    10 commit : Radio Funcionando

--- app/src/main/java/com/srcarrillo/ministerio/ui/radio/RadioFragment.java
+++ app/src/main/java/com/srcarrillo/ministerio/ui/radio/RadioFragment.java
@@ -2,6 +2,7 @@ package com.srcarrillo.ministerio.ui.radio;
 
 import androidx.lifecycle.ViewModelProviders;
 
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
@@ -11,24 +12,28 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.os.Handler;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.srcarrillo.ministerio.R;
 
+import java.io.IOException;
+
 public class RadioFragment extends Fragment {
 
     private RadioViewModel mViewModel;
     private MediaPlayer mediaPlayer ;
     private ImageView imagePlayPause;
-    private TextView textCurrentTime, textTotalDuration;
-    private SeekBar playerSeekBar;
-    private Handler handler;
+    boolean conectado;
+    ProgressBar progressBar;
+
 
     public static RadioFragment newInstance() {
         return new RadioFragment();
@@ -40,33 +45,30 @@ public class RadioFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.radio_fragment, container, false);
         imagePlayPause = view.findViewById(R.id.imagePlayPause);
-        textCurrentTime = view.findViewById(R.id.textCurrentTime);
-        //textTotalDuration = view.findViewById(R.id.textTotalDuration);
-        playerSeekBar = view.findViewById(R.id.playerSeekBar);
         mediaPlayer =new MediaPlayer();
-        handler = new Handler();
+
+        imagePlayPause.setEnabled(false);
+        progressBar = view.findViewById(R.id.progressBar);
+        progressBar.setIndeterminate(true);
+        conectado = conectar();
 
 
 
-        playerSeekBar.setMax(100);
 
         imagePlayPause.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (mediaPlayer.isPlaying()){
-                    handler.removeCallbacks(updater);
                     mediaPlayer.pause();
                     imagePlayPause.setImageResource(R.drawable.ic_play);
                 } else {
                     mediaPlayer.start();
                     imagePlayPause.setImageResource(R.drawable.ic_pause);
-                    updateSeekBar();
                 }
             }
         });
-        prepareMediaPlayer();
-
         return view;
+
     }
 
     @Override
@@ -75,51 +77,33 @@ public class RadioFragment extends Fragment {
         mViewModel = ViewModelProviders.of(this).get(RadioViewModel.class);
         // TODO: Use the ViewModel
     }
-    private void prepareMediaPlayer(){
+
+
+    public boolean conectar(){
+        Toast.makeText(getActivity().getApplicationContext(),"Intentado conectar",Toast.LENGTH_LONG).show();
+
+
+        String url = "http://stm3.miradio.com.es:12036/"; // your URL here
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
         try {
-            mediaPlayer.setDataSource("stm3.miradio.com.es:12036");
-            mediaPlayer.prepareAsync();
-            //textTotalDuration.setText(milliSecondsToTimer(mediaPlayer.getDuration()));
+            mediaPlayer.setDataSource(url);
+            mediaPlayer.prepareAsync();// might take long! (for buffering, etc)
+            mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mediaPlayer) {
+                    Toast.makeText(getActivity().getApplicationContext(),"Conectado",Toast.LENGTH_LONG).show();
+                    progressBar.setVisibility(View.GONE);
+                    imagePlayPause.setEnabled(true);
+                }
+            });
 
-        } catch (Exception exception){
-            Toast.makeText(getActivity(), exception.getMessage() ,Toast.LENGTH_LONG).show();
-
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
         }
+        return true;
     }
-    private Runnable updater = new Runnable() {
-        @Override
-        public void run() {
-            updateSeekBar();
-            long currentDuration = mediaPlayer.getCurrentPosition();
-            textCurrentTime.setText(milliSecondsToTimer(currentDuration));
-        }
-    };
-    private void updateSeekBar(){
-
-        if (mediaPlayer.isPlaying()){
-            playerSeekBar.setProgress((int) (((float) mediaPlayer.getCurrentPosition()/ mediaPlayer.getDuration())* 100));
-            handler.postDelayed(updater, 1000);
-        }
-    }
-
-   private String milliSecondsToTimer(long milliSeconds){
-        String timerString = "";
-        String secondsString;
-
-        int hours = (int)(milliSeconds / (1000*60*60));
-        int minutes = (int)(milliSeconds % (1000*60*60) ) / (1000*60);
-        int seconds = (int)((milliSeconds % (1000*60*60)) % (1000*60) / 1000);
-
-        if (hours > 0 ){
-            timerString = hours + " : ";
-        }
-        if (seconds < 10){
-            secondsString = "0" + seconds;
-        }else {
-            secondsString = "" + seconds;
-        }
-        timerString = timerString + minutes + " : " +  secondsString;
-        return timerString;
-    }
-
 }
+    
+
